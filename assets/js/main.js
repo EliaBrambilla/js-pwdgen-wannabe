c4fe66b36320caf956cb8bb6ec3ae2dcb641d275
EliaBrambilla <elia.brambilla@icloud.com>
2022-10-18 14:28:10 +0200

    aggiungo script js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
+const nomeUtente = prompt('NOME?');
 
+const cognomeUtente = prompt('COGNOME??');
+
+const colorePreferito = prompt('SCRIVI UN COLORE :)');
+
+document.getElementById('password').innerHTML +=
+`${nomeUtente}${cognomeUtente}${colorePreferito}97`;
